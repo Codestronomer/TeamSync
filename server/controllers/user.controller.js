41c026c40b718ad1c531afaa945e276b90b843b9
codestronomer <johnrumide6@gmail.com>
2023-01-06 18:33:40 +0100

    [fix] user router funtion

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -18,4 +18,15 @@ const getUser = async (req, res) => {
         }
 }
 
-export { newProfile, getUser };
+//get Users
+const getUsers = async (req, res) => {
+        try {
+                const users = await User.find();
+                if (!users) return res.status(404).json({ message: "No users found." })
+                users.password = ""
+                res.status(200).json(users);
+        } catch (err) {
+                res.status(500).json({ error: err.message })
+        }
+}
+export { newProfile, getUser, getUsers };
